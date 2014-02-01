5bedd8f0addee86e1ae53eb0aeec480fb90a100b
Angel <trigasangelos@gmail.com>
2014-02-01 16:22:02 +0000

    Applied indentation.

--- src/gm/playerRand.java
+++ src/gm/playerRand.java
@@ -1,27 +1,24 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author Angelos Trigkas
  */
+
 package gm;
 
 import java.util.*;
 
-/**
- *
- * @author Angel
- */
+
 public class playerRand {
+
     private Random rand = new Random();
-int getMove(Board cB)
-{
-List<Integer> poss= new ArrayList<Integer>();
-for(int i = 0; i <7; i++)
-{
-if(cB.validMove(i))
-{
-poss.add(i);
-}
-}
-return poss.get(Math.abs(rand.nextInt(poss.size())));
-}
+
+    int getMove(Board cB) {
+        List<Integer> poss = new ArrayList<Integer>();
+        for (int i = 0; i < 7; i++) {
+            if (cB.validMove(i)) {
+                poss.add(i);
+            }
+        }
+        return poss.get(Math.abs(rand.nextInt(poss.size())));
+    }
 }
