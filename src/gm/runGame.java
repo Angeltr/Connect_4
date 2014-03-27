7c23139d26648be64f2685145fabb7175fd8c8e8
Angel <trigasangelos@gmail.com>
2014-03-27 00:51:28 +0000

    Basic command line interface added

--- src/gm/runGame.java
+++ src/gm/runGame.java
@@ -18,15 +18,35 @@ public class runGame {
        k = new playerSearch();
 
        BufferedReader move = new BufferedReader(new InputStreamReader(System.in));
-
+       
+      System.out.println("\nWelcome to Connect 4 game!\n");
+      
+      System.out.println("Enter below a number from 0 to 6 to drop a piece in the column you wish.");
+      System.out.println("Winner is the one who first connects four pieces in row, column or diagonally.\n");
+      
+      
+      
        while ((b.winnerIs() == 0) && b.validMovesLeft()) {
            if (b.getCP() == PLAYER_ONE) {
-               b.makeMove(Integer.parseInt(move.readLine()));// Make it so!
+        	   int col;
+        	   System.out.print("User's turn. Enter column number >> ");
+        	   col = Integer.parseInt(move.readLine());
+        	   while (col < 0 || col > 6) {
+        		   System.out.print("Enter a number between 0 and 6   >> ");
+        		   col = Integer.parseInt(move.readLine());
+        	   }
+               b.makeMove(col);// Make it so!
            } else {
                b.makeMove(p.getMove(b));// Make it so!
            }
            System.out.println(b);
-
+       }
+       
+       if (b.winnerIs() == 1) {
+    	   System.out.println("You win!!!");
+       }
+       else {
+    	   System.out.println("Computer won.");
        }
 
    }
